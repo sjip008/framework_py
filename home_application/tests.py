f9f715ee3abe4564daea7d055ab6e0d91307ecb1
hsq <314629925>
2019-06-05 17:09:47 +0800

    update

--- home_application/tests.py
+++ home_application/tests.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
 # from django.test import TestCase
-
+import sys,os
+print('Python %s on %s' % (sys.version, sys.platform))
+import django;
+print('Django %s' % django.get_version())
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+sys.path.extend(['E:\\pythonproject\\framework_py', 'D:\\JetBrains\\PyCharm\\helpers\\pycharm', 'D:\\JetBrains\\PyCharm\\helpers\\pydev'])
+if 'setup' in dir(django):
+    django.setup()
 # Create your tests here.
+
+from django.conf import settings
+print(settings.__dict__)
